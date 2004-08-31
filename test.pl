aa72cd7aeac01267c7b15230e1cf5a02529b4cb0
Andy Burke <aburke@bitflood.org>
2004-08-31 07:20:09 +0000

    updates for testing

--- test.pl
+++ test.pl
@@ -2,7 +2,12 @@ use strict;
 
 use BitFlood::Encoder;
 
+my $targetFile = shift(@ARGV);
+
 my $e = new BitFlood::Encoder(filename => @ARGV > 1 ? \@ARGV : $ARGV[0],
                               weightingFunction => 'topheavy',
                               tracker => ['http://blah.com/blah', 'http://moo/yak']);
-$e->encode();
+
+open(OUTFILE, ">$targetFile");
+print OUTFILE $e->encode();
+close(OUTFILE);
