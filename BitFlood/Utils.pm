0dc547d499797ad8cab1f5aa8fdb5993f4042318
Andy Burke <aburke@bitflood.org>
2004-09-02 09:12:01 +0000

    - cleanups for relative path shit

--- BitFlood/Utils.pm
+++ BitFlood/Utils.pm
@@ -4,16 +4,17 @@ require Exporter;
 @ISA = qw(Exporter);
 @EXPORT = qw(&CleanFilename &LocalFilename &GetLocalPathFromFilename);
 
+use strict;
+
 use File::Spec;
 use File::Spec::Unix;
 
 sub CleanFilename {
-  # FIXME: check splitpath (no_file) etc. on windows
+  # FIXME: windows compatible, asshole?
   my ($volume, $dirs, $filename) = File::Spec->splitpath(shift, 1);
   my @directories = File::Spec->splitdir($dirs);
-  shift @directories if $directories[0] eq '.';
+  shift @directories if( $directories[0] eq '.' );
   return File::Spec::Unix->canonpath(join('/', @directories, $filename));
-
   #return File::Spec::Unix->catfile($volume, @directories, $filename);
 }
 
