66998f32b3ae9fa53fe062be90fd7fc997a48a1c
Andy Burke <aburke@bitflood.org>
2004-09-10 09:32:46 +0000

    - updated to reflect refactoring in Client.pm

--- testClient.pl
+++ testClient.pl
@@ -4,7 +4,16 @@ use BitFlood::Client;
 
 my $c = BitFlood::Client->new(port => $ARGV[1] || 10102);
 $c->AddFloodFile($ARGV[0]);
+
+my $UPDATE_INTERVAL = 20; #seconds
+my $last_update = 0;
+
 while(1) {
+  if (time() - $last_update >= $UPDATE_INTERVAL) {
+    $c->Register;
+    $c->UpdatePeerList;
+    $last_update = time();
+  }
   $c->GetChunks;
   $c->do_one_loop();
 }
