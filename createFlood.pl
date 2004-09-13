b2c605461d23311600db6a03458c11e776b44e48
Andy Burke <aburke@bitflood.org>
2004-09-13 21:01:46 +0000

    - added better command line options and usage

--- createFlood.pl
+++ createFlood.pl
@@ -1,13 +1,52 @@
 use strict;
 
+use Getopt::Long;
+
 use BitFlood::Encoder;
 
-my $targetFile = shift(@ARGV);
+my $floodFile;
+my $weightingFunction;
+my @trackerUrls;
+my $getHelp;
+
+GetOptions(
+	   "flood=s"     => \$floodFile,
+	   "weighting=s" => \$weightingFunction,
+	   "tracker=s"   => \@trackerUrls,
+	   "h"           => \$getHelp,
+	   "help"        => \$getHelp, 
+	   );
+
+if($getHelp) {
+  my ($appName) = $0 =~ /[\\\/]?(.*?)$/;
+  print qq{
+Usage:
+  $appName -flood <floodname.flood> -tracker <host> [-tracker <host>...] [-weighting [topheavy|bottomheavy]] <files/dirs to add>
+  };
+  exit(0);
+}
+
+
+foreach my $trackerUrl (@trackerUrls) {
+  next if($trackerUrl =~ m|^http://|);
+  my ($host, $port) = split(':', $trackerUrl);
+  die("Could not determing host from '$trackerUrl'!") if(!defined($host));
+  $port ||= 10101;
+  $trackerUrl = "http://$host:$port/RPCSERV";
+}
+
+print qq{
+  Creating Flood:
+    trackers : @trackerUrls
+    weighting: $weightingFunction
+    files    : @ARGV
+
+};
 
 my $e = new BitFlood::Encoder(filename => @ARGV > 1 ? \@ARGV : $ARGV[0],
-                              weightingFunction => 'topheavy',
-                              tracker => ['http://localhost:10101/RPCSERV']);
+                              weightingFunction => $weightingFunction,
+                              tracker => \@trackerUrls);
 
-open(OUTFILE, ">$targetFile");
+open(OUTFILE, ">$floodFile");
 print OUTFILE $e->encode();
 close(OUTFILE);
