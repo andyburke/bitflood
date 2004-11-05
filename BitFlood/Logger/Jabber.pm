385082c1967cfa64ae1a3d0d51a7493342172055
Jeremy Muhlich <jmuhlich@bitflood.org>
2004-11-05 10:45:54 +0000

    - made jabber actually work - connect just once in constructor - added infostring

--- BitFlood/Logger/Jabber.pm
+++ BitFlood/Logger/Jabber.pm
@@ -2,12 +2,16 @@ package BitFlood::Logger::Jabber;
 
 use strict;
 use base qw(BitFlood::Logger);
-__PACKAGE__->mk_accessors(qw(client));
+__PACKAGE__->mk_accessors(qw(
+  client connected infoString
+  recipient serverHost serverPort username password resource
+));
 
 use Net::Jabber qw(Client);
 
 
-# args: recipient, serverHost, serverPort, user, password, resource
+# args: recipient, serverHost, serverPort,
+#       username, password, resource, infoString
 sub new {
   my $class = shift;
   my %args = @_;
@@ -16,6 +20,17 @@ sub new {
 
   $self->client(Net::Jabber::Client->new);
   $self->client or die("failed to create Jabber Client: $!");
+  if ($self->client->Connect(hostname => $self->serverHost,
+                             port     => $self->serverPort)) {
+    my @auth = $self->client->AuthSend(username => $self->username,
+                                       password => $self->password,
+                                       resource => $self->resource);
+    if ($auth[0] eq "ok") {
+      $self->connected(1);
+    }
+  }
+
+  return $self;
 }
 
 
@@ -23,22 +38,24 @@ sub commit {
   my $self = shift;
   my $string = shift;
 
+  $self->connected or return;
+
+  my $body;
+  $body .= '[' . $self->infoString . '] ' if defined $self->infoString;
+  $body .= $string;
+
   my $im = Net::Jabber::Message->new();
-  $im->SetMessage(body => $string,
-		  type => "chat");
+  $im->SetMessage(body => $body, type => "chat");
   $im->SetTo($self->recipient);
 
-  $self->client->Connect(hostname => $self->serverHost,
-			 port     => $self->serverPort)
-      or return;
-
-  my @auth = $self->client->AuthSend(username => $self->user,
-				     password => $self->password,
-				     resource => $self->resource);
-  $auth[0] eq "ok" or return;
-
   $self->client->Send($im);
-  $self->client->Disconnect;
+}
+
+
+sub close {
+  my $self = shift;
+
+  $self->client->Disconnect if $self->connected;
 }
 
 
