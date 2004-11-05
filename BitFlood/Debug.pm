1a73c599ff8b6d4a9ab5b62205a30e452f5609c1
Andy Burke <aburke@bitflood.org>
2004-11-05 07:39:46 +0000

    - multiple channel support

--- BitFlood/Debug.pm
+++ BitFlood/Debug.pm
@@ -19,14 +19,27 @@ require Exporter;
 
 my $debugLevel;
 my $debugLineNumbers;
-my $logger = BitFlood::Logger::Multi->new();
+my %loggers; # hash on channel name
 
 SetDebugLevel($ENV{BITFLOOD_DEBUG}) if(defined($ENV{BITFLOOD_DEBUG}));
 SetDebugLineNumbers($ENV{BITFLOOD_DEBUG_LINENUMBERS}) if(defined($ENV{BITFLOOD_DEBUG_LINENUMBERS}));
 
-AddStdout() unless defined($ENV{BITFLOOD_DEBUG_QUIET});
-AddStderr() if defined($ENV{BITFLOOD_DEBUG_STDERR});
-AddFile($ENV{BITFLOOD_DEBUG_FILENAME} || 'debug.log.txt') if defined($ENV{BITFLOOD_DEBUG_FILE});  
+OpenChannel(''); # default channel
+AddLogger('', 'Stdout')
+  unless defined($ENV{BITFLOOD_DEBUG_QUIET});
+AddLogger('', 'Stderr')
+  if defined($ENV{BITFLOOD_DEBUG_STDERR});
+AddLogger('', 'File', $ENV{BITFLOOD_DEBUG_FILENAME} || 'debug.log')
+  if defined($ENV{BITFLOOD_DEBUG_FILE});  
+
+foreach my $channel (split(',', $ENV{BITFLOOD_DEBUG_CHANNELS})) {
+  OpenChannel($channel);
+}
+
+AddLogger('trace', 'Stdout');
+AddLogger('net', 'Stdout');
+AddLogger('perf', 'File', 'perf.log');
+
 
 sub SetDebugLevel {
   $debugLevel = shift;
@@ -36,25 +49,93 @@ sub SetDebugLineNumbers {
   $debugLineNumbers = shift;
 }
 
-sub AddStdout {
-  $logger->add(BitFlood::Logger::Stdout->new());
+sub OpenChannel {
+  my $channel = shift;
+  my $loggerObject = shift;
+
+  die("channel '$channel' already open") if $loggers{$channel};
+
+  if ($loggerObject) {
+    if (!UNIVERSAL::isa($loggerObject, 'BitFlood::Logger')) {
+      die("not a Logger object: $loggerObject");
+    }
+  } else {
+    $loggerObject = BitFlood::Logger::Multi->new();
+  }    
+
+  $loggers{$channel} = $loggerObject;
 }
 
-sub AddStderr {
-  $logger->add(BitFlood::Logger::Stderr->new());
+sub CloseChannel {
+  my $channel = shift;
+
+  $loggers{$channel}->close;
+  delete $loggers{$channel};
 }
 
-sub AddFile {
-  my $filename = shift;
-  $filename or return;
-  $logger->add(BitFlood::Logger::File->new($filename));
+sub MuteChannel {
+  my $channel = shift;
+
+  $loggers{$channel}->muted(1);
 }
+
+sub UnmuteChannel {
+  my $channel = shift;
+
+  $loggers{$channel}->muted(0);
+}
+
+
+sub AddLogger {
+  my $channel = shift;
+  my $logger = shift;
+  my @loggerParams = @_;
+
+  $loggers{$channel} and $loggers{$channel}->isa('BitFlood::Logger::Multi')
+    or return; # FIXME warn here?
+
+  my $loggerObject;
+
+  if (UNIVERSAL::isa($logger, 'BitFlood::Logger')) {
+    $loggerObject = $logger;
+  } else {
+    my $class = 'BitFlood::Logger::' . $logger;
+    eval "require " . $class;
+    if ($@) {
+      die "Couldn't load Logger class " . $class . " : $@";
+    }
+    $loggerObject = $class->new(@loggerParams);
+  }
+
+  $loggers{$channel}->add($loggerObject);
+}
+
 
 sub Debug {
   my $ref = shift;
-  my $level = shift || 1;
+  my ($channel, $level);
+  if (@_ == 0) {
+    $channel = '';
+    $level = 1;
+  } elsif (@_ == 1) {
+    if ($_[0] =~ /^\d+$/) {
+      $level = $_[0];
+      $channel = '';
+    } else {
+      $channel = $_[0];
+      $level = 1;
+    }
+  } elsif (@_ == 2) {
+    $channel = $_[0];
+    $level = $_[1];
+  } else {
+    warn("too many arguments to Debug");
+    return;
+  }
 
-  defined($debugLevel) and ($debugLevel >= $level) or return; # early-out
+  return unless ( defined($debugLevel)
+		  and $debugLevel >= $level
+		  and $loggers{$channel} );
 
   # we allow people to execute code at given debug levels...
   # something like:
@@ -77,7 +158,7 @@ sub Debug {
   $output .= ": ";
   $output .= BitFlood::Utils::Stringify($ref);
 
-  $logger->log($output);
+  $loggers{$channel}->log($output);
 }
 
 1;
