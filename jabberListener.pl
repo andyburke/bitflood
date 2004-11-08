311b9a2a2985fedfe917c555f923ec87fffdbfc4
Jeremy Muhlich <jmuhlich@bitflood.org>
2004-11-08 04:35:19 +0000

    - flush files every so often - refactor signal handlers and the functions they call - make stdout output more consistent

--- jabberListener.pl
+++ jabberListener.pl
@@ -7,60 +7,85 @@ use Data::Dumper;
 
 
 
-my $server     = 'jabber.org';
-my $serverPort = 5222;
-my $username   = 'bftest';
-my $password   = 'bftest';
-my $resource   = 'listener';
+my $server      = 'jabber.org';
+my $serverPort  = 5222;
+my $username    = 'bftest';
+my $password    = 'bftest';
+my $resource    = 'listener';
+my $flushPeriod = 5;
 my $help;
 my %files;
 
 if(!GetOptions(
-	       "server=s"     => \$server,
-	       "serverport=s" => \$serverPort,
-	       "username=s"   => \$username,
-	       "password=s"   => \$password,
-	       "resource=s"   => \$resource,
-	       "help"         => \$help,
+	       "server=s"      => \$server,
+	       "serverport=s"  => \$serverPort,
+	       "username=s"    => \$username,
+	       "password=s"    => \$password,
+	       "resource=s"    => \$resource,
+               "flushperiod=s" => \$flushPeriod,
+	       "help"          => \$help,
 	       )
    or $help) {
-  print "options: server serverport username password resource\n";
+  print "options: server serverport username password resource flushperiod\n";
   exit;
 }
 
 
 my $client = Net::Jabber::Client->new;
+print "== connecting to $server:$serverPort\n";
 if (!$client->Connect(hostname => $server,
 		      port     => $serverPort)) {
-  print "could not connect to server\n";
+  print "!! could not connect to server\n";
   exit;
 }
+print "== authing as $username:$password/$resource\n";
 my @auth = $client->AuthSend(username => $username,
 			     password => $password,
 			     resource => $resource);
 if ($auth[0] ne "ok") {
-  print "authentication failed: " . join(" / ", @auth) . "\n";
+  print "!! authentication failed: " . join(" / ", @auth) . "\n";
   exit;
 }
 
-$client->SetCallBacks(message => \&handle_message);
+$client->SetCallBacks(message => \&handleMessage);
 
 
-my $quit = 0;
-$SIG{INT} = sub { $quit = 1 };
+$SIG{INT} = \&sigInt;
+$SIG{ALRM} = \&sigAlarm;
 
-while (!$quit) {
+print "== will flush files every $flushPeriod seconds\n";
+alarm($flushPeriod);
+while (1) {
   my $gotData = $client->Process(1);
-  defined $gotData or $quit = 1;
+  defined $gotData or quit();
 }
 
-$client->Disconnect;
-
-exit;
 
 
+sub sigInt {
+  quit();
+}
 
-sub handle_message {
+sub sigAlarm {
+  flushFiles();
+  alarm($flushPeriod);
+}
+
+
+sub quit {
+  $client->Disconnect;
+  exit;
+}
+
+sub flushFiles {
+  print "== flushing files\n";
+  foreach my $file (values %files) {
+    $file->flush
+  }
+}
+
+
+sub handleMessage {
   my $id = shift;
   my $message = shift;
 
@@ -69,7 +94,7 @@ sub handle_message {
   my $body = $message->GetBody;
 
   if (!$files{$resource}) {
-    print "== $resource : opening file\n";
+    print "== opening file: $resource.log\n";
     $files{$resource} = IO::File->new(">>$resource.log");
   }
   $files{$resource}->print($body . "\n");
