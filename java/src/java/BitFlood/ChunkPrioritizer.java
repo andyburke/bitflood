5a863ffc9ab41412a7b08db00ad45fcb3b91256f
Luke Petre <lpetre@gmail.com>
2004-12-04 20:59:38 +0000

    moving source

---
+++ java/src/java/BitFlood/ChunkPrioritizer.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+
+/**
+ * Created on Dec 3, 2004
+ *
+ */
+
+/**
+ * @author burke
+ *
+ */
+public class ChunkPrioritizer implements Comparator
+{
+
+  public int compare(Object o1, Object o2) throws ClassCastException
+  {
+    if ( !(o1 instanceof Flood.RuntimeChunk) || !(o2 instanceof Flood.RuntimeChunk) )
+    {
+      throw new ClassCastException();
+    }
+    Flood.RuntimeChunk c1 = (Flood.RuntimeChunk) o1;
+    Flood.RuntimeChunk c2 = (Flood.RuntimeChunk) o2;
+
+    return c2.weight - c1.weight; // we want the reverse of the usual
+  }
+  
+}
